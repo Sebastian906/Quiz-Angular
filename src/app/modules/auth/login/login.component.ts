69f6e37b825caf18490367c023623ddf55abc693
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-02-22 21:25:07 -0500

    Creación de los modulos de usuario y administrador junto a los componentes de sus respectivos paneles y enrutamiento

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -38,6 +38,11 @@ export class LoginComponent {
         role: res.role
       }
       UserStorageService.saveUser(user);
+      if(UserStorageService.isAdminLoggedIn()) {
+        this.router.navigateByUrl('admin/dashboard');
+      } else if(UserStorageService.isUserLoggedIn()) {
+        this.router.navigateByUrl('user/dashboard');
+      }
       console.log(res);
     }, error => {
       this.message.error('Bad credentials', { nzDuration: 5000 });

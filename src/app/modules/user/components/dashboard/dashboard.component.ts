ecbc8bf78fcca236aca36ad7c02d678dfcffdbb4
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-03-02 22:15:11 -0500

    Se integra la API para mostrarle los cuestionarios a los usuarios, se crea el servicio para llamar a la API en los usuarios

--- src/app/modules/user/components/dashboard/dashboard.component.ts
+++ src/app/modules/user/components/dashboard/dashboard.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { TestService } from '../../services/test.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  tests = [];
+
+  constructor(private notification: NzNotificationService,
+    private testService: TestService
+  ) {}
+
+  ngOnInit() {
+    this.getAllTests();
+  }
+
+  getAllTests() {
+    this.testService.getAllTest().subscribe(res=>{
+      this.tests = res;
+    }, error=> {
+      this.notification.error('ERROR', 'Something Went Wrong. Try Again',{ nzDuration: 5000 })
+    })
+  }
+
+  getFormattedTime(time): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes} minutes ${seconds}`;
+  }
 
 }
